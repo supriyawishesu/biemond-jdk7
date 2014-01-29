d72de284348189ead0c3531be422377206de259a
supriya <supriya.uppalapati@gmail.com>
2014-01-29 15:28:08 -0500

    Update javaexec.pp

--- manifests/javaexec.pp
+++ manifests/javaexec.pp
@@ -30,54 +30,54 @@ define javaexec (
   }
 
   # check java install folder
-  if ! defined(File["/usr/java"]) {
-    file { "/usr/java" :
+  if ! defined(File["/u01/app/oracle/product/java"]) {
+    file { "/u01/app/oracle/java" :
       ensure  => directory,
     }
   }
 
   # extract gz file in /usr/java
   exec { "extract java ${fullVersion}":
-    cwd     => "/usr/java",
+    cwd     => "/u01/app/oracle/product/java",
     command => "tar -xzf ${path}/${jdkfile}",
-    creates => "/usr/java/${fullVersion}",
-    require => File["/usr/java"],
+    creates => "/u01/app/oracle/product/java/${fullVersion}",
+    require => File["/u01/app/oracle/product/java"],
   }
 
   # set permissions
-  exec { "chown -R root:root /usr/java/${fullVersion}":
+  exec { "chown -R oracle:noinstall /usr/java/${fullVersion}":
     unless  => "ls -al /usr/java/${fullVersion}/bin/java | awk ' { print \$3 }' |  grep  root",
     require => Exec["extract java ${fullVersion}"],
   }
 
 	# java link to latest
-  file { '/usr/java/latest':
+  file { '/u01/app/oracle/product/java/latest':
     ensure  => link,
-    target  => "/usr/java/${fullVersion}",
+    target  => "/u01/app/pracle/product/java/${fullVersion}",
     require => Exec["extract java ${fullVersion}"],
   }
 
 	# java link to default
-  file { '/usr/java/default':
+  file { '/u01/app/oracle/product/java/default':
     ensure  => link,
-    target  => "/usr/java/latest",
-    require => File['/usr/java/latest'],
+    target  => "/u01/app/oracle/product/java/latest",
+    require => File['/u01/app/oracle/product/java/latest'],
   }
 
   case $osfamily {
     RedHat: {
 			# set the java default
       exec { "default java alternatives ${fullVersion}":
-        command => "alternatives --install /usr/bin/java java /usr/java/${fullVersion}/bin/java ${alternativesPriority}",
-        require => File['/usr/java/default'],
+        command => "alternatives --install /u01/app/oracle/product/bin/java java /u01/app/oracle/product/java/${fullVersion}/bin/java ${alternativesPriority}",
+        require => File['/u01/app/oracle/product/java/default'],
         unless  => "alternatives --display java | /bin/grep ${fullVersion}",
       }
     }
     Debian, Suse:{
 			# set the java default
       exec { "default java alternatives ${fullVersion}":
-        command => "update-alternatives --install /usr/bin/java java /usr/java/${fullVersion}/bin/java ${alternativesPriority}",
-        require => File['/usr/java/default'],
+        command => "update-alternatives --install /u01/app/oracle/product/bin/java java /u01/app/oracle/product/java/${fullVersion}/bin/java ${alternativesPriority}",
+        require => File['/u01/app/oracle/product/java/default'],
         unless  => "update-alternatives --list java | /bin/grep ${fullVersion}",
       }
     }
